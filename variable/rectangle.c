4b043a4da25772448c64e5c4d11956ef2d3bd47e
kesar-sooryavanshi <kesarsooryavanshi@gmail.com>
2026-09-20 10:03:12 +0530

    Updated project features

---
+++ variable/rectangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+    int a , b , l;
+    printf("enter a and b: ");
+    scanf("%d %d", &a, &b);
+    l = 2*(a + b);
+    printf("l is : %d", l);
+    return 0;
+
+}
