d8996ae1cf6eced091c4d5459e5934b632bbf446
kesar-sooryavanshi <kesarsooryavanshi@gmail.com>
2026-09-22 20:15:26 +0530

    Updated project features

---
+++ chapter-2/program4.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<math.h>
+int main() {
+    int a=4+9*10;
+    printf("%d", a);
+    return 0;
+}
---
+++ chapter-2/program5.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include<math.h>
+int main() {
+    printf("%d\n", !((5>1) && (3>4)));
+    return 0;
+}
